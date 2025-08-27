4757ce513a135c6226f5539da293f4f79b27072e
Jbassard <jonathanacciarito@gmail.com>
2025-08-27 12:26:02 -0400

    init

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// serve client folder one level up
+app.use(express.static(path.join(__dirname, "..", "client")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "client", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`API server running on port ${PORT}!`);
+});
