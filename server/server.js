5d6245028ea916e200ccaaadb1bdd52e840f03fd
Jbassard <jonathanacciarito@gmail.com>
2026-01-09 21:29:24 -0500

    lol handleScannedUrl is not a function. that's because I'm bad at export and require in backend js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const handleScannedUrl = require("./bot");
+const { handleScannedUrl } = require("./bot");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
