f5353eba88b91c796294c6b02211ff59794ab3ed
Jbassard <jonathanacciarito@gmail.com>
2026-01-09 21:52:47 -0500

    there might be 9 or 10 questions on page 3, made a conditional so that the app won't crash if there's a mismatch

--- server/bot.js
+++ server/bot.js
@@ -6,17 +6,21 @@ async function handleScannedUrl(url) {
     console.log("Received QR URL:", url);
 
     const browser = await chromium.launch({
-      headless: true,
+      headless: false,
       slowMo: 50,
       args: ["--no-sandbox", "--disable-setuid-sandbox"],
     });
 
     const page = await browser.newPage();
+    console.log("On 1st page...");
     await page.goto(url, { waitUntil: "networkidle" });
-
     await page.getByRole("button", { name: "Next" }).click();
+
+    console.log("On 2nd page...");
     await page.getByText("Highly Satisfied").click();
     await page.getByRole("button", { name: "Next" }).click();
+
+    console.log("on 3rd page...");
     await page.locator("label").first().click();
     await page
       .locator("div:nth-child(2) > .surv-answ > div > div:nth-child(2) > label")
@@ -42,12 +46,16 @@ async function handleScannedUrl(url) {
     await page
       .locator("div:nth-child(9) > .surv-answ > div > div:nth-child(2) > label")
       .click();
-    await page
-      .locator(
-        "div:nth-child(10) > .surv-answ > div > div:nth-child(2) > label"
-      )
-      .click();
+    const tenthHighlySatisfied = page.locator(
+      "div:nth-child(10) > .surv-answ > div > div:nth-child(2) > label"
+    );
+    if ((await tenthHighlySatisfied.count()) > 0) {
+      console.log("Tenth 'Highly Satisfied' found, clicking it...");
+      await tenthHighlySatisfied.click();
+    }
     await page.getByRole("button", { name: "Next" }).click();
+
+    console.log("On 4th page...");
     await page.getByText("Highly Satisfied").first().click();
     await page.getByText("Highly Satisfied").nth(1).click();
     await page.getByText("Highly Satisfied").nth(2).click();
@@ -71,13 +79,14 @@ async function handleScannedUrl(url) {
     await browser.close();
     return "Success";
   } catch (err) {
+    await browser.close();
     console.error("Bot encountered an error:", err);
     return `Failure (${err.message})`;
   }
 }
 
-// handleScannedUrl(
-//   "https://survey.order.marcos.com/survey?id=TzZKd0VGOFJodFJaUW9VdWhRclczZz09&src=2"
-// );
+handleScannedUrl(
+  "https://survey.order.marcos.com/survey?id=T2R6dVBGT3I5SHBWZ2tOL0dWem1HUT09%22"
+);
 
 module.exports = { handleScannedUrl };
